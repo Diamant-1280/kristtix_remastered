fba2805df2d593ea2c9418da6fb474b7dccd21a8
Diamant 1280 <52082821+Diamant-1280@users.noreply.github.com>
2022-03-13 16:49:14 +0400

    хероку, я иду к тебе!!

--- src/app/global.d.ts
+++ src/app/global.d.ts
@@ -1,4 +1,11 @@
+import { Guild_Interface, User_Interface } from '@interfaces/MongoDB'
+
 declare global {
+    namespace Intl {
+        class ListFormat {
+            public format: (items: string[]) => string;
+        }
+    }
     namespace NodeJS {
         interface ProcessEnv {
             token: string
@@ -7,6 +14,32 @@ declare global {
             PORT: string
         }
     }
-}
 
+    interface String {
+        capitalize(): string;
+
+        clear(): string;
+
+        translate<T>(this: string, vars: T): string;
+
+        chunk(this: string, len: number): string[];
+    }
+
+    interface Boolean {
+        parse(val: string): boolean
+    }
+
+    interface Array<T> {
+        shuffled(): T[]
+    }
+}
+declare module 'discord.js' {
+    export interface Guild {
+        settings: Guild_Interface
+    }
+
+    export interface GuildMember {
+        options: User_Interface
+    }
+}
 export {}
