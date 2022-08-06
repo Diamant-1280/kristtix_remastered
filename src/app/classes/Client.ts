4629b4773f54372b43ccbd8ade9fea88665d1998
Diamant 1280 <52082821+Diamant-1280@users.noreply.github.com>
2022-08-06 18:01:33 +0300

    Update Client.ts

--- src/app/classes/Client.ts
+++ src/app/classes/Client.ts
@@ -51,45 +51,6 @@ export class ExtendedClient extends Client {
             })
         })
 
-        /* this.on("messageCreate", (message) => {
-            async function Eval(message: Message): Promise<void> {
-                const args: string[] = message.content.slice(prefix.length + 1).trim().split(" ")
-                const code: string = args.join(" ")
-                async function clean(code: string): Promise<string> {
-                    if (code && code.constructor.name == "Promise")
-                        code = await code;
-                    if (typeof code !== "string")
-                        code = require("util").inspect(code, { depth: 0 })
-                    code = code.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-                    return code;
-                }
-
-                try {
-                    const evaled: any = args.includes("await") ? eval(`(async () => { ${code} })()`) : eval(code), cleaned = await clean(evaled);
-                    const text: string = cleaned.length > 2000 ? "Текст содержит более 2к символов блэт. Выслан ебучий файл." : cleaned;
-                    const file: any = cleaned.length > 2000 ? [new AttachmentBuilder(Buffer.from(`${cleaned}`), { name: "HelloMyFirstOtchim.js" })] : [];
-                    await message.reply({ content: `\`\`\`js\n${text}\n\`\`\``, files: file })
-                } catch (err) {
-                    message.channel.send({ content: `\`ERROR\`\n\`\`\`x1\n${err}\n\`\`\`` })
-                }
-            this.registerCommands({
-                    commands: slashCommands,
-                    guildId: message.guild.id
-
-                })}
-
-            const prefix = '~'
-            if (message.content.startsWith(prefix + 'e') && this.owners.includes(message.author.id)) Eval.call(this, (message))
-            if (message.content.startsWith(prefix + 'setup') && this.owners.includes(message.author.id)) {
-                this.registerCommands({
-                    commands: slashCommands,
-                    guildId: message.guild.id
-
-                })
-            }
-        // }) */
-
-
         const eventFiles: string[] = await globPromise(`${__dirname}/../events/*{.ts,.js}`)
         eventFiles.forEach(async filePath => {
             const event: Event<keyof ClientEvents> = await this.importFile(filePath)
