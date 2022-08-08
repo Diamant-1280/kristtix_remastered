52084d40d1a97c6a995b26568ad069de7e0ae6d8
Diamant 1280 <52082821+Diamant-1280@users.noreply.github.com>
2022-08-08 22:53:37 +0400

    Update vote.ts

--- src/app/commands/information/vote.ts
+++ src/app/commands/information/vote.ts
@@ -1,4 +1,5 @@
 import { Command } from "@classes/Command";
+import { ApplicationCommandOptionType } from "discord.js";
 
 export default new Command({
     name: "vote",
@@ -6,5 +7,45 @@ export default new Command({
     description: "Создает сообщение с голосованием",
     run: ({ args }) => {
         console.log(args)
+
+
     },
+    options: [
+        {
+            name: "question",
+            nameLocalizations: { ru: "вопрос" },
+            description: "Укажите сообщение с вопросом",
+            type: ApplicationCommandOptionType.String,
+            required: true
+        }, {
+            name: "1st-answer",
+            nameLocalizations: {ru: "первый ответ"},
+            description: "Укажите текст первого ответа",
+            type: ApplicationCommandOptionType.String,
+            required: true
+        },
+        {
+            name: "2nd-answer",
+            nameLocalizations: {ru: "второй ответ"},
+            description: "Укажите текст первого ответа",
+            type: ApplicationCommandOptionType.String,
+            required: true
+        },
+        {
+            name: "3rd-answer",
+            nameLocalizations: {ru: "третий ответ"},
+            description: "Укажите текст первого ответа",
+            type: ApplicationCommandOptionType.String,
+        }, {
+            name: "4th-answer",
+            nameLocalizations: {ru: "четвертый ответ"},
+            description: "Укажите текст первого ответа",
+            type: ApplicationCommandOptionType.String,
+        }, {
+            name: "5th-answer",
+            nameLocalizations: {ru: "пятый ответ"},
+            description: "Укажите текст первого ответа",
+            type: ApplicationCommandOptionType.String,
+        }
+    ]
 })
